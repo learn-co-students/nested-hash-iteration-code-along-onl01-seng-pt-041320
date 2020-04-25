fc0f931fbde64d5f7ab188e195aea93d8fd1dc42
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-25 20:19:59 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,3 +1,4 @@
+
 require 'pry'
 
   # This is the array we will be passing into the remove_strawberry method
@@ -15,5 +16,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+contacts.each do |person, contacts|
+  contacts.each do |attribute, value|
+    if attribute == :favorite_ice_cream_flavors
+      value.delete_if {|ice_cream| ice_cream == "strawberry"}
+    end
+   end 
+ end
 end
